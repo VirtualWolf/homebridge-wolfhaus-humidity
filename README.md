45ca3e4cee61e3e4a679f8e7e64987d5f0f75501
VirtualWolf <virtualwolf@virtualwolf.org>
2020-04-12 07:52:34 +0100

    Split out humidity into separate Homebridge plugin so it shows as a distinct panel in Home.app.

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# homebridge-wolfhaus
+# homebridge-wolfhaus-humidity
 
-An extremely simple plugin for Homebridge to pull the current temperature from an HTTP endpoint and display it in Home on iOS.
+An extremely simple plugin for Homebridge to pull the current humidity from an HTTP endpoint and display it in Home on iOS.
 
 Example Homebridge `config.json` accessory configuration:
 
 ```
 "accessories": [
     {
-        "accessory": "Wolfhaus",
-        "name": "Outdoor Temperature",
+        "accessory": "WolfhausHumidity",
+        "name": "Outdoor Humidity",
         "url": "http://pi:3000/rest/outdoor"
     },
     {
-        "accessory": "Wolfhaus",
-        "name": "Indoor Temperature",
+        "accessory": "WolfhausHumidity",
+        "name": "Indoor Humidity",
         "url": "http://fourbee:3000/rest/indoor"
     }
 ],
--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-var request = require('superagent');
-var Service, Characteristic;
+const request = require('superagent');
+let Service, Characteristic;
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
 
-    homebridge.registerAccessory("homebridge-wolfhaus", "Wolfhaus", WolfhausAccessory);
+    homebridge.registerAccessory("homebridge-wolfhaus-humidity", "WolfhausHumidity", WolfhausHumidityAccessory);
 }
 
-class WolfhausAccessory {
+class WolfhausHumidityAccessory {
     constructor(log, config) {
         this.log = log;
-        this.log('Adding WolfhausAccessory');
+        this.log('Adding WolfhausHumidityAccessory');
         this.name = config.name;
         this.url = config.url;
 
-        this.wolfhausTemperatureService = new Service.TemperatureSensor(this.name);
-        this.wolfhausTemperatureService
-            .getCharacteristic(Characteristic.CurrentTemperature)
-            .on('get', this.getCurrentTemperature);
+        this.wolfhausHumidityService = new Service.HumiditySensor(this.name);
+        this.wolfhausHumidityService
+            .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .on('get', this.getCurrentHumidity);
     }
 
     getServices = () => {
-        return [this.wolfhausTemperatureService];
+        return [this.wolfhausHumidityService];
     }
 
-    getCurrentTemperature = (callback) => {
+    getCurrentHumidity = (callback) => {
         request.get(this.url)
-            .then(result => callback(null, result.body.temperature));
+            .then(result => callback(null, result.body.humidity));
     }
 }
